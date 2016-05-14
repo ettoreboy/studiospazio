00c696cbd35fccedcc2df690992b12c6a97aa258
Ettore Ciprian <cipettaro@gmail.com>
2016-05-14 16:52:35 +0200

    error noscript

--- js/main.js
+++ js/main.js
@@ -2,74 +2,30 @@
 * @author Ettore Ciprian [cipettaro@gmail.com]
 */
 
-/**
- * load Configurations from a Json file
- */
-function loadConfig(filepath){
-  $.ajaxSetup({beforeSend: function(xhr){
-      if (xhr.overrideMimeType)
-      {
-        xhr.overrideMimeType("application/json");
-      }
+function updateText (id) {
+  var currentId = flkty.selectedElement.id;
+  if (flkty.selectedIndex >= 0 && currentId !== id) {
+    $("div.textgallery #" + id).hide()
+    if (open_text) {
+      $('div.textgallery div#'+currentId).fadeIn()
     }
-    });
-     //Load config
-    return $.getJSON(filepath, function(json) {
-      return json;
-     });
-}
-
-//Load images with jquery
-function loadImage(id, path, target) {
-          $('<img id="img_'+id+'" class="img-resp" src="'+ path +'">').load(function() {
-            $(this).appendTo(target);
-        });
+    if (open_plan) {
+      $('div.textgallery img#'+currentId).fadeIn(1000);
     }
-
-function createGalleryItem(id, path){
-
-  $('<img class="img-resp lazyload" data-sizes="auto" id="'+id+'"/>').attr("data-src", path).attr("src", path).appendTo("#lightgallery");
-}
-
-function loadFile(file) {
-var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
-    reader.open('get', file, true);
-    reader.onreadystatechange = displayContents;
-    reader.send(null);
-}
-
-function displayContents(objectID) {
-    if(reader.readyState==4) {
-        var el = document.getElementById(objectID);
-        el.innerHTML = reader.responseText;
-    }
-}
-
-
-function updateText(id){
-  var currentId =  flkty.selectedElement.id;
-  if(flkty.selectedIndex >= 0 && currentId !== id){
-    $("div.textgallery #"+id).hide();
-   if(open_text){
-     $('div.textgallery div#'+currentId).fadeIn();
-   }
-   if(open_plan){
-     $('div.textgallery img#'+currentId).fadeIn(1000);
-   }
   }
 }
 
-function toggleText(element, show_text, show_plan){
-  if(show_text){
+function toggleText (element, show_text, show_plan) {
+  if (show_text) {
     $(".overlay").fadeIn();
-    $('div.textgallery div#'+element.id).fadeIn();
-  }else if(!show_text){
-     $(".overlay").fadeOut();
-     $('div.textgallery div#'+element.id).fadeOut();
+    $('div.textgallery div#'+ element.id).fadeIn()
+  }else if (!show_text) {
+    $(".overlay").fadeOut();
+    $('div.textgallery div#'+ element.id).fadeOut()
   }
-  if(show_plan){
-    $('div.textgallery img#'+element.id).fadeIn(1000);
-  }else if(!show_plan){
-    $('div.textgallery img#'+element.id).fadeOut();
+  if (show_plan) {
+    $('div.textgallery img#'+ element.id).fadeIn(1000)
+  }else if (!show_plan) {
+    $('div.textgallery img#'+ element.id).fadeOut()
   }
 }
